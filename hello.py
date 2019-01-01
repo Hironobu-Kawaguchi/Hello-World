782ff6f7d2239d76ee8720bc2d01d9938af997d3
Hironobu-Kawaguchi <40334053+Hironobu-Kawaguchi@users.noreply.github.com>
2019-01-01 16:24:16 +0900

    comment out

--- hello.py
+++ hello.py
@@ -6,3 +6,9 @@ Created on Tue Jun 19 10:48:08 2018
 """
 
 print("Hello Anaconda")
+
+# %matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0,15,200)
+plt.plot(x,np.sin(x))

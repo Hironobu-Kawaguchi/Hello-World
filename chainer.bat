20eac6778acef4694520de5337bad2a6b6df49fe
Hironobu Kawaguchi <40334053+Hironobu-Kawaguchi@users.noreply.github.com>
2019-01-28 22:21:15 +0900

    move

--- chainer.bat
+++
@@ -1,4 +0,0 @@
-call C:\ProgramData\Anaconda3\Scripts\activate chainer
-python %USERPROFILE%\GitHub\Hello-World\chainer_mnist.py
-call C:\ProgramData\Anaconda3\Scripts\deactivate
-pause
--- tensorflow_intro.bat
+++
@@ -1,4 +0,0 @@
-call C:\ProgramData\Anaconda3\Scripts\activate tensorflow
-python %USERPROFILE%\GitHub\Hello-World\tensorflow_intro.py
-call C:\ProgramData\Anaconda3\Scripts\deactivate
-pause

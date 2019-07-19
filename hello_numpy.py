fbc2b6df5b75cb227b88a2cecb35ec66815ddaff
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2019-07-19 15:42:33 +0900

    test seed

--- hello_numpy.py
+++ hello_numpy.py
@@ -3,6 +3,11 @@ import numpy as np
 def main():
     print("Hello NumPy")
     print("np.__version__", np.__version__)
+    # np.random.seed(42)
+    # print(np.random.get_state())
+    print(np.random.random(1))
 
 if __name__ == '__main__':
+    np.random.seed(42)
     main()
+    # print(np.random.random(1))

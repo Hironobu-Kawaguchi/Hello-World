0e036c083af2c072787f5e959c9cc1a420616e54
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2019-07-19 18:01:27 +0900

    add logger

--- _template.py
+++ _template.py
@@ -1,34 +1,54 @@
 import os
 import argparse
 import numpy as np
-random_state=42
-np.random.seed(random_state)
 import pandas as pd
-# pd.set_option('display.max_columns', None)  # 常に全ての列（カラム）を表示
-# pd.set_option('display.max_rows', None)     # 常に全ての行を表示
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 matplotlib.style.use('ggplot')
+from logging import StreamHandler, DEBUG, Formatter, FileHandler, getLogger
 
+logger = getLogger(__name__)
 
 def main(args):
     # Create directories if not exist.
+    logger.debug('start')
     if not os.path.exists(args.result_dir):
         os.makedirs(args.result_dir)
+    logger.debug('end')
 
     return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='description of this program')
     parser.add_argument('--verbose', type=int, default=0, help='verbose')
+    parser.add_argument('--seed', type=int, default=42, help='Numpy Random State')
     parser.add_argument('--batch_size', type=int, default=16, help='mini-batch size')
     parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'])
     parser.add_argument('--result_dir', type=str, default='results')
     parser.add_argument('--foobar', action='store_true')    # default -> False, --foobar  -> True
     parser.add_argument('--foonley', action='store_false')  # default -> True,  --foonley -> False
-
     args = parser.parse_args()
     if args.verbose:
         print(args)
+
+    log_fmt = Formatter('%(asctime)s %(name)s %(lineno)d [%(levelname)s][%(funcName)s] %(message)s ')
+    handler = StreamHandler()
+    handler.setLevel('INFO')
+    handler.setFormatter(log_fmt)
+    logger.addHandler(handler)
+
+    handler = FileHandler(os.path.join(args.result_dir, '_template.py.log'), 'a')
+    handler.setLevel(DEBUG)
+    handler.setFormatter(log_fmt)
+    logger.setLevel(DEBUG)
+    logger.addHandler(handler)
+
+    logger.info('start')
+
+    np.random.seed(args.seed)
+    logger.debug('np.random.seed: {}'.format(args.seed))
+
     main(args)
+
+    logger.info('end')
